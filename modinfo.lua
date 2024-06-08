ce5e39bd62ecb79b09477fff848e22f8c174c483
liolok <i@liolok.com>
2024-06-08 16:07:07 +0800

    重构代码

--- modinfo.lua
+++ modinfo.lua
@@ -1,6 +1,6 @@
-name = "中文字体：苍耳玄三"
-author = "Skull, gcc, EvenMr, 幽晚枫乐, iaman2b & 李皓奇"
-version = "2024.06.04.1"
+name = '中文字体：苍耳玄三'
+author = 'Skull, gcc, EvenMr, 幽晚枫乐, iaman2b & 李皓奇'
+version = '2024.06.04.1'
 description = [[
 󰀏 制作栏物品的皮肤名称字体的解决方案：
 
@@ -17,52 +17,52 @@ description = [[
 api_version = 10
 priority = -2022
 
-icon_atlas = "modicon.xml"
-icon = "modicon.tex"
+icon_atlas = 'modicon.xml'
+icon = 'modicon.tex'
 
 dst_compatible = true
 client_only_mod = true
 all_clients_require_mod = false
 
 configuration_options = {
-    {
-        name = "font_scale_ratio",
-        label = "缩放倍率",
-        options = {
-            {description = "0.80", data = 0.80},
-            {description = "0.85", data = 0.85},
-            {description = "0.90", data = 0.90},
-            {description = "0.95", data = 0.95},
-            {description = "1.00", data = 1.00},
-            {description = "1.05", data = 1.05},
-            {description = "1.10", data = 1.10},
-            {description = "1.15", data = 1.15},
-            {description = "1.20", data = 1.20},
-        },
-        default = 1.00,
+  {
+    name = 'font_scale_ratio',
+    label = '缩放倍率',
+    options = {
+      { description = '0.80', data = 0.80 },
+      { description = '0.85', data = 0.85 },
+      { description = '0.90', data = 0.90 },
+      { description = '0.95', data = 0.95 },
+      { description = '1.00', data = 1.00 },
+      { description = '1.05', data = 1.05 },
+      { description = '1.10', data = 1.10 },
+      { description = '1.15', data = 1.15 },
+      { description = '1.20', data = 1.20 },
     },
-    {
-        name = "world_clock_size",
-        label = "世界时钟",
-        hover = "世界天数、幸存天数的字号",
-        options = {
-            {description = "30", data = 30},
-            {description = "31", data = 31},
-            {description = "32", data = 32},
-            {description = "33", data = 33},
-            {description = "34", data = 34},
-            {description = "35", data = 35},
-            {description = "36", data = 36},
-            {description = "37", data = 37},
-            {description = "38", data = 38},
-            {description = "39", data = 39},
-            {description = "40", data = 40},
-            {description = "41", data = 41},
-            {description = "42", data = 42},
-            {description = "43", data = 43},
-            {description = "44", data = 44},
-            {description = "45", data = 45},
-        },
-        default = 32,
+    default = 1.00,
+  },
+  {
+    name = 'world_clock_size',
+    label = '世界时钟',
+    hover = '世界天数、幸存天数的字号',
+    options = {
+      { description = '30', data = 30 },
+      { description = '31', data = 31 },
+      { description = '32', data = 32 },
+      { description = '33', data = 33 },
+      { description = '34', data = 34 },
+      { description = '35', data = 35 },
+      { description = '36', data = 36 },
+      { description = '37', data = 37 },
+      { description = '38', data = 38 },
+      { description = '39', data = 39 },
+      { description = '40', data = 40 },
+      { description = '41', data = 41 },
+      { description = '42', data = 42 },
+      { description = '43', data = 43 },
+      { description = '44', data = 44 },
+      { description = '45', data = 45 },
     },
+    default = 32,
+  },
 }
--- modmain.lua
+++ modmain.lua
@@ -1,100 +1,82 @@
-local _G = GLOBAL
+local G = GLOBAL
 
-local fonts = {
-    "belisaplumilla_outline",
-    "bellefair_outline",
-    "bellefair",
-    "hammerhead",
-    "spirequal_outline",
-    "spirequal",
-    "stint_outline"
+local fonts = { -- 加入了苍耳玄三的原版字体列表
+  'belisaplumilla_outline',
+  'bellefair_outline',
+  'bellefair',
+  'hammerhead',
+  'spirequal_outline',
+  'spirequal',
+  'stint_outline'
 }
 
-local assets = {}
-for _, v in pairs(fonts) do table.insert(assets, _G.Asset("FONT", MODROOT.."fonts/"..v..".zip")) end
-
-local replaces = {
-    TITLEFONT               = "belisaplumilla_outline",
-    UIFONT                  = "belisaplumilla_outline",
-    TALKINGFONT             = "belisaplumilla_outline",
-    TALKINGFONT_HERMIT      = "belisaplumilla_outline",
-    TALKINGFONT_TRADEIN     = "belisaplumilla_outline",
-    TALKINGFONT_WORMWOOD    = "belisaplumilla_outline",
-    CHATFONT_OUTLINE        = "bellefair_outline",
-    CHATFONT                = "bellefair",
-    BUTTONFONT              = "bellefair",
-    HEADERFONT              = "hammerhead",
-    NEWFONT_OUTLINE         = "spirequal_outline",
-    NEWFONT_OUTLINE_SMALL   = "spirequal_outline",
-    NEWFONT                 = "spirequal",
-    NEWFONT_SMALL           = "spirequal",
-    BODYTEXTFONT            = "stint_outline",
-    NUMBERFONT              = "stint_outline",
-    SMALLNUMBERFONT         = "stint_outline",
+local replaces = { -- 字体替换列表
+  TITLEFONT             = 'belisaplumilla_outline',
+  UIFONT                = 'belisaplumilla_outline',
+  TALKINGFONT           = 'belisaplumilla_outline',
+  TALKINGFONT_HERMIT    = 'belisaplumilla_outline',
+  TALKINGFONT_TRADEIN   = 'belisaplumilla_outline',
+  TALKINGFONT_WORMWOOD  = 'belisaplumilla_outline',
+  CHATFONT_OUTLINE      = 'bellefair_outline',
+  CHATFONT              = 'bellefair',
+  BUTTONFONT            = 'bellefair',
+  HEADERFONT            = 'hammerhead',
+  NEWFONT_OUTLINE       = 'spirequal_outline',
+  NEWFONT_OUTLINE_SMALL = 'spirequal_outline',
+  NEWFONT               = 'spirequal',
+  NEWFONT_SMALL         = 'spirequal',
+  BODYTEXTFONT          = 'stint_outline',
+  NUMBERFONT            = 'stint_outline',
+  SMALLNUMBERFONT       = 'stint_outline',
 }
 
-local function RegisterFonts()
-    local prefab = "tsanger_xuansan"
-    for _, font in pairs(fonts) do _G.TheSim:UnloadFont(font) end
-    _G.TheSim:UnloadPrefabs({prefab})
-    _G.TheSim:RegisterPrefab(prefab, assets, {})
-    _G.TheSim:LoadPrefabs({prefab})
-    for _, font in pairs(fonts) do
-        _G.TheSim:LoadFont(MODROOT.."fonts/"..font..".zip", font)
-        _G.TheSim:SetupFontFallbacks(font, font:find("outline")
-            and _G.DEFAULT_FALLBACK_TABLE_OUTLINE or _G.DEFAULT_FALLBACK_TABLE)
-    end
-    for k, v in pairs(replaces) do _G[k] = v end
+local assets = {} -- 资源文件列表
+for _, font in pairs(fonts) do table.insert(assets, Asset('FONT', MODROOT .. 'fonts/' .. font .. '.zip')) end
+
+local function RegisterFonts() -- 注册字体
+  for _, font in pairs(fonts) do G.TheSim:UnloadFont(font) end
+  G.TheSim:UnloadPrefabs { 'tsanger_xuansan' }
+  G.TheSim:RegisterPrefab('tsanger_xuansan', assets, {})
+  G.TheSim:LoadPrefabs { 'tsanger_xuansan' }
+  for _, font in pairs(fonts) do
+    local fallback = font:find 'outline' and G.DEFAULT_FALLBACK_TABLE_OUTLINE or G.DEFAULT_FALLBACK_TABLE
+    G.TheSim:LoadFont(MODROOT .. 'fonts/' .. font .. '.zip', font)
+    G.TheSim:SetupFontFallbacks(font, fallback)
+  end
+  for k, v in pairs(replaces) do G[k] = v end
 end
 
-local OldRegisterPrefabs = _G.ModManager.RegisterPrefabs
-_G.ModManager.RegisterPrefabs = function(...)
-    OldRegisterPrefabs(...)
-    RegisterFonts()
+local OldRegisterPrefabs = G.ModManager.RegisterPrefabs
+G.ModManager.RegisterPrefabs = function(...)
+  OldRegisterPrefabs(...)
+  RegisterFonts()
 end
 
-local OldStart = _G.Start
-_G.Start = function()
-    RegisterFonts()
-    OldStart()
+local OldStart = G.Start
+G.Start = function()
+  RegisterFonts()
+  OldStart()
 end
 
 -- 缩放倍率
-
-local ratio = GetModConfigData("font_scale_ratio")
-
-_G.LOC.GetTextScale = function() -- 仅对客户端语言为简体中文的情况调整缩放
-    return (_G.LANGUAGE.CHINESE_S == _G.LOC.CurrentLocale.id) and ratio or _G.LOC.CurrentLocale.scale
+local ratio = GetModConfigData 'font_scale_ratio'
+G.LOC.GetTextScale = function() -- 仅对客户端语言为简体中文的情况调整缩放
+  return (G.LANGUAGE.CHINESE_S == G.LOC.CurrentLocale.id) and ratio or G.LOC.CurrentLocale.scale
 end
 
--- 以下调节一些较小的字体
-
-AddClassPostConstruct("screens/pausescreen", function(self)
-    if self.subtitle then self.subtitle:SetSize(24) end
+-- 细节微调
+AddClassPostConstruct('screens/pausescreen', function(self) if self.subtitle then self.subtitle:SetSize(24) end end)
+AddClassPostConstruct('screens/redeemdialog', function(self) if self.fineprint then self.fineprint:SetSize(24) end end)
+AddClassPostConstruct('widgets/badge', function(self)
+  if self.num then self.num:SetSize(24 / ratio) end
+  if self.maxnum then self.maxnum:SetSize(20 / ratio) end
 end)
-
-AddClassPostConstruct("screens/redeemdialog", function(self)
-    if self.fineprint then self.fineprint:SetSize(24) end
+AddClassPostConstruct('widgets/controls', function(self)
+  if self.seasonclock and self.seasonclock._text then self.seasonclock._text:SetSize(34 / ratio) end
 end)
-
-AddClassPostConstruct("widgets/badge", function(self)
-    if self.num then self.num:SetSize(24 / ratio) end
-    if self.maxnum then self.maxnum:SetSize(20 / ratio) end
-end)
-
-AddClassPostConstruct("widgets/controls", function(self)
-    if self.seasonclock and self.seasonclock._text then self.seasonclock._text:SetSize(34 / ratio) end
-end)
-
-AddClassPostConstruct("widgets/ingredientui", function(self)
-    if self.quant then self.quant:SetSize(32 / ratio) end
-end)
-
-AddClassPostConstruct("widgets/skincollector", function(self)
-    if self.text then self.text:SetSize(26 / ratio) end
-end)
-
-AddClassPostConstruct("widgets/uiclock", function(self)
-    if self._text then self._text:SetSize(GetModConfigData("world_clock_size") / ratio) end
-    if self._moonanim and self._moonanim.moontext then self._moonanim.moontext:SetSize(18 / ratio) end
+AddClassPostConstruct('widgets/ingredientui', function(self) if self.quant then self.quant:SetSize(32 / ratio) end end)
+AddClassPostConstruct('widgets/skincollector', function(self) if self.text then self.text:SetSize(26 / ratio) end end)
+AddClassPostConstruct('widgets/uiclock', function(self)
+  if self._text then self._text:SetSize(GetModConfigData 'world_clock_size' / ratio) end
+  if self._moonanim and self._moonanim.moontext then self._moonanim.moontext:SetSize(18 / ratio) end
 end)
