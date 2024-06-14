cc17c1778006d347a581c453b2daf82fb9bd3e8b
liolok <i@liolok.com>
2024-06-14 23:41:58 +0800

    使用 StyLua 重新格式化

--- modmain.lua
+++ modmain.lua
@@ -5,24 +5,24 @@ local fonts = { -- 加入了苍耳玄三的原版字体列表
   'bellefair_outline',
   'bellefair',
   'hammerhead',
+  'hennypenny_outline',
   'spirequal_outline',
   'spirequal',
   'stint_outline',
   'sugarplum_outline',
-  'hennypenny_outline'
 }
 
+-- stylua: ignore
 local replaces = { -- 字体替换列表
   TITLEFONT             = 'belisaplumilla_outline',
   UIFONT                = 'belisaplumilla_outline',
   TALKINGFONT           = 'belisaplumilla_outline',
-  TALKINGFONT_HERMIT    = 'sugarplum_outline',
   TALKINGFONT_TRADEIN   = 'belisaplumilla_outline',
-  TALKINGFONT_WORMWOOD  = 'hennypenny_outline',
   CHATFONT_OUTLINE      = 'bellefair_outline',
   CHATFONT              = 'bellefair',
   BUTTONFONT            = 'bellefair',
   HEADERFONT            = 'hammerhead',
+  TALKINGFONT_WORMWOOD  = 'hennypenny_outline', -- 沃姆伍德（植物人）
   NEWFONT_OUTLINE       = 'spirequal_outline',
   NEWFONT_OUTLINE_SMALL = 'spirequal_outline',
   NEWFONT               = 'spirequal',
@@ -30,22 +30,29 @@ local replaces = { -- 字体替换列表
   BODYTEXTFONT          = 'stint_outline',
   NUMBERFONT            = 'stint_outline',
   SMALLNUMBERFONT       = 'stint_outline',
+  TALKINGFONT_HERMIT    = 'sugarplum_outline', -- 寄居蟹隐士
 }
 
 local assets = {} -- 资源文件列表
-for _, font in pairs(fonts) do table.insert(assets, Asset('FONT', MODROOT .. 'fonts/' .. font .. '.zip')) end
+for _, font in pairs(fonts) do
+  table.insert(assets, Asset('FONT', MODROOT .. 'fonts/' .. font .. '.zip'))
+end
 
 local function RegisterFonts() -- 注册字体
-  for _, font in pairs(fonts) do G.TheSim:UnloadFont(font) end
-  G.TheSim:UnloadPrefabs { 'tsanger_xuansan' }
-  G.TheSim:RegisterPrefab('tsanger_xuansan', assets, {})
-  G.TheSim:LoadPrefabs { 'tsanger_xuansan' }
   for _, font in pairs(fonts) do
-    local fallback = font:find 'outline' and G.DEFAULT_FALLBACK_TABLE_OUTLINE or G.DEFAULT_FALLBACK_TABLE
+    G.TheSim:UnloadFont(font)
+  end
+  G.TheSim:UnloadPrefabs({ 'tsanger_xuansan' })
+  G.TheSim:RegisterPrefab('tsanger_xuansan', assets, {})
+  G.TheSim:LoadPrefabs({ 'tsanger_xuansan' })
+  for _, font in pairs(fonts) do
+    local fallback = font:find('outline') and G.DEFAULT_FALLBACK_TABLE_OUTLINE or G.DEFAULT_FALLBACK_TABLE
     G.TheSim:LoadFont(MODROOT .. 'fonts/' .. font .. '.zip', font)
     G.TheSim:SetupFontFallbacks(font, fallback)
   end
-  for k, v in pairs(replaces) do G[k] = v end
+  for FONT, font in pairs(replaces) do
+    G[FONT] = font
+  end
 end
 
 local OldRegisterPrefabs = G.ModManager.RegisterPrefabs
@@ -61,14 +68,18 @@ G.Start = function()
 end
 
 -- 缩放倍率
-local ratio = GetModConfigData 'font_scale_ratio'
+local ratio = GetModConfigData('font_scale_ratio')
 G.LOC.GetTextScale = function() -- 仅对客户端语言为简体中文的情况调整缩放
   return (G.LANGUAGE.CHINESE_S == G.LOC.CurrentLocale.id) and ratio or G.LOC.CurrentLocale.scale
 end
 
 -- 细节微调
-AddClassPostConstruct('screens/pausescreen', function(self) if self.subtitle then self.subtitle:SetSize(24) end end)
-AddClassPostConstruct('screens/redeemdialog', function(self) if self.fineprint then self.fineprint:SetSize(24) end end)
+AddClassPostConstruct('screens/pausescreen', function(self)
+  if self.subtitle then self.subtitle:SetSize(24) end
+end)
+AddClassPostConstruct('screens/redeemdialog', function(self)
+  if self.fineprint then self.fineprint:SetSize(24) end
+end)
 AddClassPostConstruct('widgets/badge', function(self)
   if self.num then self.num:SetSize(24 / ratio) end
   if self.maxnum then self.maxnum:SetSize(20 / ratio) end
@@ -76,9 +87,13 @@ end)
 AddClassPostConstruct('widgets/controls', function(self)
   if self.seasonclock and self.seasonclock._text then self.seasonclock._text:SetSize(34 / ratio) end
 end)
-AddClassPostConstruct('widgets/ingredientui', function(self) if self.quant then self.quant:SetSize(32 / ratio) end end)
-AddClassPostConstruct('widgets/skincollector', function(self) if self.text then self.text:SetSize(26 / ratio) end end)
+AddClassPostConstruct('widgets/ingredientui', function(self)
+  if self.quant then self.quant:SetSize(32 / ratio) end
+end)
+AddClassPostConstruct('widgets/skincollector', function(self)
+  if self.text then self.text:SetSize(26 / ratio) end
+end)
 AddClassPostConstruct('widgets/uiclock', function(self)
-  if self._text then self._text:SetSize(GetModConfigData 'world_clock_size' / ratio) end
+  if self._text then self._text:SetSize(GetModConfigData('world_clock_size') / ratio) end
   if self._moonanim and self._moonanim.moontext then self._moonanim.moontext:SetSize(18 / ratio) end
 end)
