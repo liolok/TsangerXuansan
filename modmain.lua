170fafaf744b380e5ac233eabc6393fe5fe588bb
liolok <i@liolok.com>
2025-12-26 01:05:42 +0800

    回退字体替换方式以修复关闭模组闪退

--- modmain.lua
+++ modmain.lua
@@ -1,8 +1,5 @@
 local G = GLOBAL
 
-local hermit = GetModConfigData('enable_hermit_font')
-local wormwood = GetModConfigData('enable_wormwood_font')
-
 local fonts = { -- 加入了中文字体的原版字体列表
   'belisaplumilla_outline',
   'bellefair_outline',
@@ -16,53 +13,63 @@ local fonts = { -- 加入了中文字体的原版字体列表
 }
 
 -- stylua: ignore
-local replace = { -- 替换列表
-  ['bellefair']               = 'bellefair',
-  ['bellefair_outline']       = 'bellefair_outline',
-  ['bp100']                   = 'belisaplumilla_outline',
-  ['bp50']                    = 'belisaplumilla_outline',
-  ['buttonfont']              = 'bellefair',
-  ['hammerhead']              = 'hammerhead',
-  ['opensans']                = 'stint_outline',
-  ['spirequal']               = 'spirequal',
-  ['spirequal_small']         = 'spirequal',
-  ['spirequal_outline']       = 'spirequal_outline',
-  ['spirequal_outline_small'] = 'spirequal_outline',
-  ['stint-ucr']               = 'stint_outline',
-  ['stint-small']             = 'stint_outline',
-  ['talkingfont']             = 'belisaplumilla_outline',
-  ['talkingfont_tradein']     = 'belisaplumilla_outline',
-  ['talkingfont_hermit']      = hermit and 'sugarplum_outline' or 'belisaplumilla_outline',
-  ['talkingfont_wormwood']    = wormwood and 'hennypenny_outline' or 'belisaplumilla_outline',
+local font_table = {
+  DEFAULTFONT           = 'stint_outline',
+  DIALOGFONT            = 'stint_outline',
+  TITLEFONT             = 'belisaplumilla_outline',
+  UIFONT                = 'belisaplumilla_outline',
+  BUTTONFONT            = 'bellefair',
+  NEWFONT               = 'spirequal',
+  NEWFONT_SMALL         = 'spirequal',
+  NEWFONT_OUTLINE       = 'spirequal_outline',
+  NEWFONT_OUTLINE_SMALL = 'spirequal_outline',
+  NUMBERFONT            = 'stint_outline',
+  TALKINGFONT           = 'belisaplumilla_outline',
+  TALKINGFONT_WORMWOOD  = GetModConfigData('enable_wormwood_font') and 'hennypenny_outline' or 'belisaplumilla_outline',
+  TALKINGFONT_TRADEIN   = 'belisaplumilla_outline',
+  TALKINGFONT_HERMIT    = GetModConfigData('enable_hermit_font') and 'sugarplum_outline' or 'belisaplumilla_outline',
+  CHATFONT              = 'bellefair',
+  HEADERFONT            = 'hammerhead',
+  CHATFONT_OUTLINE      = 'bellefair_outline',
+  SMALLNUMBERFONT       = 'stint_outline',
+  BODYTEXTFONT          = 'stint_outline',
 }
+
+Assets = {} -- 资源列表
+for _, font in ipairs(fonts) do
+  local file_path = MODROOT .. 'fonts/' .. font .. '.zip'
+  local fb = G.DEFAULT_FALLBACK_TABLE
+  if font:find('_outline$') then fb = G.DEFAULT_FALLBACK_TABLE_OUTLINE end
+  table.insert(Assets, Asset('FONT', file_path))
+  table.insert(G.FONTS, { filename = file_path, alias = 'tsanger_' .. font, fallback = fb })
+end
 
 -- 注入字体
 local function ReloadFonts()
-  Assets = {}
-  for _, v in ipairs(G.FONTS) do
-    local font = replace[v.alias]
-    if font then
-      local filename = MODROOT .. 'fonts/' .. font .. '.zip'
-      table.insert(Assets, Asset('FONT', filename))
-      v.filename = filename
-      G.TheSim:UnloadFont(v.alias)
-    end
+  for _, font in ipairs(fonts) do
+    G.TheSim:UnloadFont('tsanger_' .. font)
   end
   G.TheSim:UnregisterPrefabs({ 'tsanger_fonts' })
   G.TheSim:RegisterPrefab('tsanger_fonts', Assets, {})
   G.TheSim:LoadPrefabs({ 'tsanger_fonts' })
   G.LoadFonts()
+  for k, v in pairs(font_table) do
+    G[k] = 'tsanger_' .. v
+  end
 end
+
 local OldUnregisterAllPrefabs = G.Sim.UnregisterAllPrefabs
 G.Sim.UnregisterAllPrefabs = function(...)
   OldUnregisterAllPrefabs(...)
   return ReloadFonts()
 end
+
 local OldRegisterPrefabs = G.ModManager.RegisterPrefabs
 G.ModManager.RegisterPrefabs = function(...)
   OldRegisterPrefabs(...)
   return ReloadFonts()
 end
+
 local OldStart = G.Start
 G.Start = function()
   ReloadFonts() -- 此处会概率触发字体错乱，克雷全责。
